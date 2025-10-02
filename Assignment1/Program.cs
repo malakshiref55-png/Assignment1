fcd66cedea54488671744d7508cab986e37672dc
Hdmax <Hdmax@DESKTOP-14GE2N3>
2025-10-02 23:25:13 +0300

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //-- 
+        //int a = 10, b = 2;
+        //Console.WriteLine(a + b);
+        //Console.WriteLine(a - b);
+        //Console.WriteLine(a * b);
+        // Console.WriteLine(a / b);
+        //--
+        // int x = 25, y = 20;
+        // Console.WriteLine(x == y ? "Equal" : "Not Equal");
+        //--
+        // int age = 28, salary = 7000;
+        // Console.WriteLine(age > 18 && salary > 5000 ? "True" : "False");
+        //--
+        // int x = 8;
+        // int y = 2;
+        //int z = x;
+        //Console.WriteLine(z);
+        //x+= y;
+        //Console.WriteLine(x);
+        //--
+        //int x = 5;
+        //int y = 3;
+        //x = y;
+        //x++;
+        //y--;
+        //Console.WriteLine(x);
+        //Console.WriteLine(y);
+        //--
+        // int num = 6;
+        //Console.WriteLine(num % 2 == 0 ? "Even" : "Odd");
+        //--
+        //string name = null;
+        //string displayName = name ?? "Malak";
+        //Console.WriteLine(displayName);
+        //string m = null;
+        //m ??= "rana";
+        //Console.WriteLine(m);
+        //--
+        // string name = "Mohra";
+        //int age = 22;
+        //Console.WriteLine("Name: " + name + " age: " + age);
+        //----
+        // DateTime today = DateTime.Today;
+        //Console.WriteLine($"Today : {today: dd/MM/yyyy}");
+        //----
+        //  string s1 = "Hi";
+        //string s2 = s1.Replace("Hi", "welcome");
+        //Console.WriteLine(s2);
+        
+        //--
+        // int grade =60;
+        //Console.WriteLine(grade >= 50 ? "Pass" : "Fail");
+        //---
+        //int n1 = 1;
+        //switch (n1)
+        //{
+        //  case 1: Console.WriteLine("One"); break;
+        //case 2: Console.WriteLine("Two"); break;
+        //case 3: Console.WriteLine("Three"); break;
+        //}
+        //--
+        // int n2 = 3;
+        //string result = n2 switch { 1 => "One", 2 => "Two", 3 => "Three", _ => "Other" };
+        //Console.WriteLine(result);
+        //---
+        // for (int i = 1; i <= 10; i++)
+        //{
+        ////  Console.WriteLine(i);
+        //}
+        //---
+        //string[] names = { "Malak", "rana", "mohra", "Mahi", "koky" };
+        //foreach (var name in names)
+        //{
+        // Console.WriteLine(name);
+        //}
+
+        //-----------------
+        //int sum = 0;
+        //  int number = -1; 
+        //while (number != 0)
+        //{
+        //  Console.Write("Enter a number (0 to stop): ");
+        //number = int.Parse(Console.ReadLine()); 
+        //sum += number; 
+        //}
+        //Console.WriteLine("Total sum = " + sum);
+        //---------
+        //  int[,] arr = new int[3, 3];
+        //int num = 1;
+        //for (int i = 0; i < 3; i++)
+        //{
+        //  for (int j = 0; j < 3; j++)
+        //{
+        //  arr[i, j] = num++;
+        //Console.WriteLine(arr[i, j] + " ");
+        //}
+        //Console.WriteLine();
+        //}
+        
+
+    }
+
+}
+
+   
